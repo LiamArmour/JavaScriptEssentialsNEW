52fcb6ea31ebc5e6645efbf0e6b60bb60a5db2c7
Liam Armour <liamarmour1991@googlemail.com>
2015-09-02 13:52:13 +0100

    finished task 2

---
+++ numbers/2.convert-celsius-into-farenheit.js
@@ -0,0 +1,7 @@
+'use strict';
+function convertDegreesToFarenheit (){
+    var celsiusValue = 12;
+    var farenheitValue = ((celsiusValue * 2) +30);
+    console.log(celsiusValue +" degrees celsius equates to: "+farenheitValue + " "+ "farenheit");
+}
+convert();
