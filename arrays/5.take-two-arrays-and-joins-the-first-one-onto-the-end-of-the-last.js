2c93d1e948120fab03ffc53de2ec7627acfe2f8a
Liam Armour <liamarmour1991@googlemail.com>
2015-09-02 14:12:17 +0100

    finished task five

---
+++ arrays/5.take-two-arrays-and-joins-the-first-one-onto-the-end-of-the-last.js
@@ -0,0 +1,6 @@
+'use strict'
+var names = ["Liam", "Laurie", "Keith", "Kath", "Ian", "DanPotts", "Andrew", "Dom", "Kane"];
+var finishedValue = names.concat(["BMW", "Audi"]);
+
+console.log(finishedValue);
+
