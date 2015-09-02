01abd75b3b32266c5e317b01ff1d5733b9f2118f
Liam Armour <liamarmour1991@googlemail.com>
2015-09-02 14:06:13 +0100

    finished task two

---
+++ functions-and-styling/2.love-calculator.js
@@ -0,0 +1,13 @@
+'use strict'
+var calculation = function(boyName, girlName) {
+    var foo = (boyName.length + girlName.length)-(Math.random()*6);
+    var result = foo * 42;
+
+    if(result > 100) {
+        console.log(boyName + " loves " + girlName + " 100%")
+    } else {
+        console.log(boyName + " loves " + girlName  +  result + "%")
+    }
+}
+
+calculation("Liam", "Lau");
