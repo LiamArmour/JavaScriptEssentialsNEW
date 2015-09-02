70b87e34e997075f417a1fe2aa94dd7d9a9c1fea
Liam Armour <liamarmour1991@googlemail.com>
2015-09-02 14:02:39 +0100

    finished task 3

---
+++ comparsion-and-logical-operators/3.grading-system.js
@@ -0,0 +1,17 @@
+var gradeScore = 61;
+
+if (gradeScore < 40) {
+    grade = "F";
+} else if(gradeScore < 50) {
+    grade = "E";
+} else if(gradeScore < 60) {
+    grade = "D";
+} else if(gradeScore < 70) {
+    grade = "C";
+} else if(gradeScore < 80) {
+    grade = "B";
+} else if(gradeScore < 90) {
+    grade = "A";
+}
+
+console.log(grade);
