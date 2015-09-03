41ad1b5c6e7d6fa658629d09d7cc8628a4ec897f
Liam Armour <liamarmour1991@googlemail.com>
2015-09-03 10:48:39 +0100

    made changes

--- comparsion-and-logical-operators/3.grading-system.js
+++ comparsion-and-logical-operators/3.grading-system.js
@@ -3,16 +3,18 @@ var gradeScore = 79,
 
 if (gradeScore < 40) {
     grade = "F";
-} else if(gradeScore < 50) {
+}
+else if(gradeScore < 50) {
     grade = "E";
-} else if(gradeScore < 60) {
+}
+else if(gradeScore < 60) {
     grade = "D";
-} else if(gradeScore < 70) {
+}
+else if(gradeScore < 70) {
     grade = "C";
-} else if(gradeScore < 80) {
+}
+else if(gradeScore < 80) {
     grade = "B";
-} else{
-
 }
 
 console.log(grade);
